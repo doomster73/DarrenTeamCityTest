cba1827d56e800d596c294d33b5864ad54cd973d
Darren Matthews <traycedoom@hotmail.com>
2017-05-17 18:53:52 +0100

    Initial Commit

---
+++ TeamCityTest2/TeamCityTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityTest2.Startup))]
+namespace TeamCityTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
